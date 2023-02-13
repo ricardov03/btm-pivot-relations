bebc5a51d60584f7bb306c3226f11df67a5badf4
Ricardo Vargas <im@ricardovargas.me>
2023-02-13 15:05:04 -0400

    Adding the Parent column.

--- database/migrations/2023_02_09_120643_create_properties_table.php
+++ database/migrations/2023_02_09_120643_create_properties_table.php
@@ -16,6 +16,7 @@ class CreatePropertiesTable extends Migration
         Schema::create('properties', function (Blueprint $table) {
             $table->id();
             $table->string('name', 100);
+            $table->foreignId('parent')->nullable();
         });
     }
 
